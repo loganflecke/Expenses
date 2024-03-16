35e3c7685ca28961faeb1f196541edff52b4b81e
Logan <97614921+loganflecke@users.noreply.github.com>
2024-03-16 16:24:59 -0400

    Update expenses.py

--- expenses.py
+++ expenses.py
@@ -1,94 +1,95 @@
 from flask import Flask, render_template, request
 import pandas as pd
-from datetime import datetime, timedelta
 import os
-from functools import reduce
-
-app = Flask(__name__)
 
 ## THESE ARE HEADER NAMES STRAIGHT FROM THE CSV. CHANGE THEM TO FIT YOUR FORMATTING
 category = 'Category'
 retailer = 'Description'
 cost = 'Debit'
 date = 'Transaction Date'
-myCard = ## CHANGE TO THE LAST 4 DIGITS OF YOUR CARD
 
 ## THESE ARE THE NAMES OF GROCERIES AS THEY APPEAR ON THE TRANSACTIONS CSV FILE
-grocery = ['KROGER', 'wholefoods', 'traderjoes', 'gianteagle', 'aldis']
+grocery_keywords = ['KROGER', 'wholefoods', 'traderjoes', 'gianteagle', 'aldis']
 
-## OUTPUT FILES. FEEL FREE TO REMOVE
-combined = "combined.csv"
-filtered = "filtered.csv"
-categorized = "categorized.csv"
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         try:
-            num_days = request.form['num_days']
-            transaction_path = request.form['transaction_path']
+            start_date = request.form.get('start_date')
+            end_date = request.form.get('end_date')
+            transaction_path = request.form.get('transaction_path')
 
             input_files = [f for f in os.listdir(transaction_path) if os.path.isfile(os.path.join(transaction_path, f))]
 
-            merged_df = merge_csv(transaction_path, input_files)
-            filtered_df = filter_date_and_card(num_days, merged_df)
-            categories_total_df = filter_csv_categories(filtered_df)
-            grouped_categories = groupby_categories(filtered_df, categories_total_df)
-            categories_text = print_data(categories_total_df)
-            transactions_text = print_data(filtered_df)
-            sum_text = filtered_df[cost].sum()
-            date_range_text = str(format_date(filtered_df[date].iloc[-1]) + " - " + format_date(filtered_df[date].iloc[0]))
+            merged_transactions = merge_transactions(transaction_path, input_files)
+            filtered_transactions = filter_transaction_by_date(start_date, end_date, merged_transactions)
+            categories_summary = summarize_categories(filtered_transactions)
+            grouped_categories_text = generate_category_group_text(filtered_transactions, categories_summary)
+            categories_text = generate_category_text(categories_summary)
+            transactions_text = generate_transaction_text(filtered_transactions)
+            total_cost = filtered_transactions[cost].sum().round(decimals=2)
+            date_range_text = f"{format_date(start_date)} - {format_date(end_date)}"
+            return render_template('index.html', date_range_text=date_range_text, total_cost=total_cost, grouped_categories_text=grouped_categories_text, categories_text=categories_text, transactions_text=transactions_text, start_date=start_date, end_date=end_date, transaction_path=transaction_path)
 
-            return render_template('index.html', date_range_text=date_range_text, sum_text=sum_text, grouped_categories=grouped_categories, categories_text=categories_text, transactions_text=transactions_text, num_days=num_days, transaction_path=transaction_path)
-
+        except FileNotFoundError as e:
+            error_message = f"Error: {str(e)}"
+            return render_template('index.html', error=error_message)
         except Exception as e:
-            result_text = f"An error occurred: {str(e)}"    
-            return render_template('index.html', date_range_text=date_range_text, sum_text=sum_text, grouped_categories=grouped_categories, categories_text=categories_text, transactions_text=transactions_text)
+            error_message = f"An error occurred: {str(e)}"    
+            return render_template('index.html', error=error_message)
     elif request.method == 'GET':
         return render_template('index.html')
 
 def format_date(date_value):
     return pd.to_datetime(date_value).strftime('%B %d, %Y')
 
-def merge_csv(transaction_path, input_files):
-    dfs = [pd.read_csv(os.path.join(transaction_path, file)) for file in input_files]
-    merged_df = pd.concat(dfs).drop_duplicates(subset=[retailer, cost, date])
-    merged_df.to_csv(combined, index=False)
-    return merged_df
+def merge_transactions(transaction_path, input_files):
+    transaction_dfs = [pd.read_csv(os.path.join(transaction_path, file)) for file in input_files]
+    merged_transactions = pd.concat(transaction_dfs).drop_duplicates(subset=[retailer, cost, date])
+    return merged_transactions
 
-def filter_date_and_card(num_days, merged_df):
+def filter_transaction_by_date(start_date, end_date, merged_transactions):
     date_format = "%Y-%m-%d"
-    merged_df.iloc[:, 1] = pd.to_datetime(merged_df.iloc[:, 1], format=date_format)
-    cutoff_date = datetime.now() - timedelta(days=int(num_days))
-    filtered_df = merged_df[merged_df.iloc[:, 1] >= cutoff_date]
-    filtered_df = filtered_df[filtered_df.iloc[:, 2] == myCard]
-    filtered_df = filtered_df.sort_values(by=[date], ascending=False)
-    filtered_df.to_csv(filtered, index=False)
-    return filtered_df
+    merged_transactions.iloc[:, 1] = pd.to_datetime(merged_transactions.iloc[:, 1], format=date_format)
+    
+    filtered_transactions = merged_transactions[(merged_transactions.iloc[:, 1] >= pd.to_datetime(start_date)) & 
+                            (merged_transactions.iloc[:, 1] <= pd.to_datetime(end_date))]
 
-def filter_csv_categories(filtered_df):
-    for i in grocery:
-        condition_grocery = filtered_df[retailer].str.contains(i, case=False, na=False)
-        condition_not_fuel = ~filtered_df[retailer].str.contains('FUEL', case=False, na=False)
-        if condition_grocery.any() and condition_not_fuel.any():
-            filtered_df.loc[condition_grocery & condition_not_fuel, category] = 'Grocery'
+    filtered_transactions.iloc[:, 1] = pd.to_datetime(filtered_transactions.iloc[:, 1], format=date_format)
 
-    categories_total_df = filtered_df.groupby(category)[cost].sum().round(2).reset_index()
-    categories_total_df.to_csv(categorized, index=False)
-    return categories_total_df
+    filtered_transactions = filtered_transactions.sort_values(by=date, ascending=False)
+    return filtered_transactions
 
-def groupby_categories(filtered_df, categories_total_df):
+def summarize_categories(filtered_transactions):
+    for keyword in grocery_keywords:
+        contains_keyword = filtered_transactions[retailer].str.contains(keyword, case=False, na=False)
+        not_fuel = ~filtered_transactions[retailer].str.contains('FUEL', case=False, na=False)
+        if contains_keyword.any() and not_fuel.any():
+            filtered_transactions.loc[contains_keyword & not_fuel, category] = 'Grocery'
+
+    categories_summary = filtered_transactions.groupby(category)[cost].sum().round(2).reset_index()
+    return categories_summary
+
+def generate_category_group_text(filtered_transactions, categories_summary):
     result_text = ""
-    for i,r in categories_total_df.iterrows():
-        category_df = filtered_df[filtered_df.iloc[:, 4] == categories_total_df.iloc[i, 0]]
-        result_text += f"\n{categories_total_df.iloc[i, 0]}:\n"
-        for index, row in category_df.iterrows():
-            result_text += f"{row[retailer].ljust(30)} {row[cost]}\n"
+    for i, category_row in categories_summary.iterrows():
+        category_df = filtered_transactions[filtered_transactions[category] == categories_summary.iloc[i, 0]]
+        result_text += f"\n{categories_summary.iloc[i, 0]}:\n"
+        for index, transaction_row in category_df.iterrows():
+            result_text += f"{transaction_row[retailer].ljust(30)} {transaction_row[cost]}\n"
     return result_text
 
-def print_data(dataframe):
+def generate_category_text(categories_summary):
     result_text = ""
-    for index, row in dataframe.iterrows():
+    for i, category_row in categories_summary.iterrows():
+        result_text += f"{category_row[category].ljust(30)} {category_row[cost]}\n"
+    return result_text
+
+def generate_transaction_text(filtered_transactions):
+    result_text = ""
+    for index, row in filtered_transactions.iterrows():
         try:
             result_text += f"{row[date].ljust(20)} {row[category].ljust(20)} {row[retailer].ljust(30)} {row[cost]}\n"
         except:
